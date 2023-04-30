dcca8d0ad287a73043465853ee07bf75e6d94f28
OOP-Java <mahbub1999@gmail.com>
2023-04-29 22:42:21 -0500

    Project added cucumber

--- src/test/java/smarttech/ny/myrunner/TestRunner.java
+++ src/test/java/smarttech/ny/myrunner/TestRunner.java
@@ -1,5 +1,20 @@
 package smarttech.ny.myrunner;
 
-public class TestRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
+//How do configure testNg with Cucumber? 
+// Ans: with the help of "extends AbstractTestNGCucumberTests"
+// How to run runner class? Ans: by create .xml file 
+
+@CucumberOptions(
+		//  plugin = {""}, // generate the report
+		features = {".//Feature/"},// location of feature file
+		glue = {"smarttech.ny.stepdefinitions"}  // location of step definition file 
+		)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+	  
+	
 }
+   
